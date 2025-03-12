ef015c38c7071e6fbb5926f2f7c794af6a94b4ba
Ethan Crawford <ethan.crawford5532@gmail.com>
2025-03-12 15:16:35 -0700

    style

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // // take the time at the moment the page is loaded, display it as the "current session"
-  // const dateTimeString = new Date().toLocaleString();
-  // const [datePart, timePart] = dateTimeString.split(', ');
-  // document.getElementById('current-session').innerHTML = `Current Session:<br>${datePart}<br>${timePart}`;
-
-  // // real time clock display: update the time every second
-  // setInterval(() => {
-  //   const dateTimeString = new Date().toLocaleString();
-  //   const [datePart, timePart] = dateTimeString.split(', ');
-  //   document.getElementById('real-time-clock').innerHTML = `Current Time:<br>${datePart}<br>${timePart}`;
-  // }, 1000);
-
   // fetch music portfolio data from the server
   const dataSource = "https://ecrawford4-assets.s3.us-east-2.amazonaws.com/data.json";
 
